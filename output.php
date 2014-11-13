d93f0a9d9ac4521c5e5ac761c7240ebbeb46520d
freejoe76 <joe.murphy@gmail.com>
2014-11-12 20:32:51 -0700

    Added Ski Resort death data to the slope report.

--- output.php
+++ output.php
@@ -141,6 +141,14 @@ if ( $_SERVER['argv'][1] == 'skiarea' ):
                                 unset($templateinput);
                                 $templateinput = array(
                                         'templatename'     => 'row.resort.html',
+                                        'key'     => '<a href="http://www.denverpost.com/ci_22820133/colorado-skiers-die-groomed-blue-runs-after-hitting">Slope Deaths, 2007-2012</a>',
+                                        'value'     => $row['deaths_2007_2012'] 
+                                );
+                                $datatmp .=  template($templateinput);
+
+                                unset($templateinput);
+                                $templateinput = array(
+                                        'templatename'     => 'row.resort.html',
                                         'key'     => 'Acres Open',
                                         'value'     => $row['acresopen'] . ' of ' . $row['terrain_acres']
                                 );
