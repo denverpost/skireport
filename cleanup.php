4227d2608e7006159e29c3a8aa9c8583c900307c
freejoe76 <joe.murphy@gmail.com>
2014-11-12 18:05:27 -0700

    More cleanup cleanup

--- cleanup.php
+++ cleanup.php
@@ -24,54 +24,46 @@ $ids_file = file_get_contents('ids.txt');
 $ids = explode("\n", trim($ids_file));
 
 foreach ( $ids as $value ):
-	if ( $value != '' )
-	{
-		$diff_count = 0;
+	if ( $value != '' ):
 
+		$diff_count = 0;
 		$sql = 'SELECT * FROM report WHERE skiarea_id = ' . $value . ' ORDER BY timestamp DESC LIMIT 2';
 		$result = $db->query($sql);
-		//$row = $db->fetch($result);
 
 
-		while ( $row = $db->fetch($result, 'assoc') )
-		{
+		while ( $row = $db->fetch($result, 'assoc') ):
 			$row_compare[] = $row;
-
 	//		$$slug .= '	<dd><a href="' . $url . '" title="' . $label . '">' . $label . '</a></dd>
+        endwhile;
 
-		}
 		//$diff = array_diff_assoc($row_compare[0], $row_compare[1]);
 		// row_compare[0] is the most-recent result
-		foreach ( $row_compare[0] as $keyc => $valuec )
-		{
-			if ( $keyc != 'timestamp' && ( $valuec != $row_compare[1][$keyc] ) )
-			{
+		foreach ( $row_compare[0] as $keyc => $valuec ):
+			if ( $keyc != 'timestamp' && ( $valuec != $row_compare[1][$keyc] ) ):
 				$keys[] = $keyc;
 				$values['new'][] = ( is_int($valuec) ) ? $valuec : "'" . $valuec . "'";
 				$values['old'][] = ( is_int($row_compare[1][$keyc]) ) ? $row_compare[1][$keyc] : "'" . $row_compare[1][$keyc] . "'";
 				//echo "0: $valuec 1: " . $row_compare[1][$keyc] . "<br>";
 				if ( $keyc != 'report_id' ) $diff_count ++;
-			}
-		}
+			endif;
+		endforeach;
 
-		if ( $diff_count > 0 )
-		{
+		if ( $diff_count > 0 ):
 			$sql_keys_bit = implode(', ', $keys);
 			$sql_values_new_bit = implode(', ', $values['new']);
 			$sql_values_old_bit = implode(', ', $values['old']);
 			$sql_delta[] = 'INSERT INTO report_delta ( skiarea_id, new, ' . $sql_keys_bit . ' ) VALUES ( ' . $value . ', 0, ' . $sql_values_old_bit . ' ),
 		(  ' . $value . ', 1, ' . $sql_values_new_bit . ' );
 	';
-		}
-		else
-		{
+		else:
 			$sql_delete[] = 'DELETE FROM report WHERE skiarea_id = ' . $value . ' ORDER BY timestamp DESC LIMIT 1;
 	';
-		}
+		endif;
+
 		//echo $value . ': ' . $diff_count . '<hr>';
 		unset($row_compare); unset($keys); unset($values);
 		//var_dump($row_compare);
-	}
+	endif;
 
 endforeach;
 
