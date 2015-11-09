9cb94bae911e43e5935001780310fa85dda52d93
freejoe76 <jmurphy@denverpost.com>
2015-11-09 17:36:59 -0500

    Clarify name

--- cron.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /var/www/apache/denverpostplus.com/httpdocs/app/skireport/
-source .source.bash && echo $DB_USER > dblog
-./update.bash update > log-update-bash
-date > log-skiphp
-/usr/bin/php output.php skiarea >> log-skiphp
